30d0063c5b49d3eb1ea27b737ae452b727ebd420
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-12-13 17:29:34 +0100

    Init loans microservice

---
+++ loans/src/test/java/io/github/wkktoria/microbank/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.microbank.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
